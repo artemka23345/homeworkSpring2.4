2e031fb403925271709308e01e8e95f5b9bf3741
Inv <artem@tarabakin.ru>
2023-11-28 02:51:31 +0300

    Add plus service

---
+++ src/main/java/com/artemka23345/homeworkSpring/CalculatorController.java
@@ -0,0 +1,26 @@
+package com.artemka23345.homeworkSpring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+        private СalculatorService сalculatorService;
+
+    public CalculatorController(СalculatorService сalculatorService) {
+        this.сalculatorService = сalculatorService;
+    }
+@GetMapping()
+    public String greetings(){
+        return сalculatorService.greetings();
+    }
+    @GetMapping("/plus")
+    public String plus(@RequestParam("num1") Integer num1,@RequestParam("num2") Integer num2){
+        return сalculatorService.plus(num1,num2);
+    }
+
+
+}
---
+++ src/main/java/com/artemka23345/homeworkSpring/СalculatorService.java
@@ -0,0 +1,17 @@
+package com.artemka23345.homeworkSpring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class СalculatorService {
+
+    public String greetings(){
+        return "<b>Добро пожаловать в калькулятор</b>";
+    }
+    public String plus(Integer num1,Integer num2){
+        return num1 + "+" + num2 + "=" + (num1 + num2);
+
+
+    }
+}
+
