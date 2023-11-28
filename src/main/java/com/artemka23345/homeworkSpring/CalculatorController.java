c0cfa8f8824537cc5db26505ee0bc47ad288d1fb
Inv <artem@tarabakin.ru>
2023-11-28 10:27:42 +0300

    Done

--- src/main/java/com/artemka23345/homeworkSpring/CalculatorController.java
+++ src/main/java/com/artemka23345/homeworkSpring/CalculatorController.java
@@ -5,25 +5,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
-        private СalculatorService сalculatorService;
 
-    public CalculatorController(СalculatorService сalculatorService) {
+    private final CalculatorService сalculatorService;
+
+    public CalculatorController(CalculatorService сalculatorService) {
         this.сalculatorService = сalculatorService;
     }
-@GetMapping()
+
+    @GetMapping("/")
     public String greetings(){
         return сalculatorService.greetings();
     }
     @GetMapping("/plus")
-    public String plus(@RequestParam("num1") Integer num1,@RequestParam("num2") Integer num2){
-        return сalculatorService.plus(num1,num2);
+    public String plus(@RequestParam(required = false) Integer num1,@RequestParam(required = false) Integer num2){
+        if(num1 == null || num2 == null){
+            return "Не передан параметер";
+        }
+        return  num1 + "+" + num2 + "=" + сalculatorService.plus(num1,num2);
+    }
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(required = false) Integer num1,@RequestParam(required = false) Integer num2){
+        if(num1 == null || num2 == null){
+            return "Не передан параметр";
+        }
+        return  num1 + "*" + num2 + "=" + сalculatorService.multiply(num1,num2);
     }
     @GetMapping("/minus")
-    public String minus(@RequestParam("num1") Integer num1,@RequestParam("num2") Integer num2){
-        return сalculatorService.plus(num1,num2);
+    public String minus(@RequestParam(required = false) Integer num1,@RequestParam(required = false) Integer num2){
+        if(num1 == null || num2 == null){
+            return "Не передан параметр";
+        }
+        return  num1 + "+" + num2 + "=" + сalculatorService.minus(num1,num2);
+    }
+    @GetMapping("/divide")
+    public String divide(@RequestParam(required = false) Double num1,@RequestParam(required = false) Double num2){
+        if(num1 == null || num2 == null){
+            return "Не передан параметр";
+        }
+        if(num2 == 0){
+            return "Деление на ноль";
+        }
+        return  num1 + "/" + num2 + "=" + сalculatorService.divide(num1,num2);
     }
 
 
---
+++ src/main/java/com/artemka23345/homeworkSpring/CalculatorService.java
@@ -0,0 +1,9 @@
+package com.artemka23345.homeworkSpring;
+
+public interface CalculatorService {
+    String greetings();
+    Integer plus(Integer num1,Integer num2);
+    Integer minus(Integer num1,Integer num2);
+    Integer multiply(Integer num1,Integer num2);
+    Double divide(Double num1,Double num2);
+}
